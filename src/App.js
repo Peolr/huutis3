699daa75b1e8e0909c67d4e4ffefcb5b00d3fc72
peolr <ber.fr@outlook.com>
2024-01-05 00:09:15 +0200

    yes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,233 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef, useEffect } from 'react';
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/interaction';
+import fiLocale from '@fullcalendar/core/locales/fi';
+import ReactModal from 'react-modal';
+ReactModal.setAppElement('#root');
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function Calendar() {
+    // ... other state variables ...
+    const [isChecked, setIsChecked] = useState(false);
+    const calendarRef = useRef(null);
+    const [isEditing, setIsEditing] = useState(false);
+    const [selectedEvent, setSelectedEvent] = useState(null);
+    const [selectedDate, setSelectedDate] = useState(null);
+    const [modalVisible, setModalVisible] = useState(false);
+    const [events, setEvents] = useState();
+    const [daysEvents, setDaysEvents] = useState();
+    const handleDeleteEvent = () => {
+        fetch(`http://localhost:3001/events/${selectedEvent.id}`, {
+            method: 'DELETE',
+        })
+        .then(response => response.json())
+        .then(data => {
+            if (data.error) throw new Error(data.error);
+            console.log('Success:', data);
+            // Remove the deleted event from the events state
+            const updatedEvents = events.filter(event => event.id !== selectedEvent.id);
+            setEvents(updatedEvents);
+            setModalVisible(false);
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+    };
+    
+
+    const handleEventClick = async (info) => {
+        console.log("Event clicked: ", info);
+        const eventId = info.event.id;
+    
+        try {
+            const response = await fetch(`http://localhost:3001/events/${eventId}`);
+            const event = await response.json();
+            console.log("Event: ", event);
+            setSelectedEvent({ id: eventId, title: event.title, description: event.description, starttime: event.starttime, ketatulos: event.ketatulos, start: event.start });
+            setModalVisible(true);
+            setIsEditing(true);
+        } catch (error) {
+            console.error("Error:", error);
+        }
+    }
+
+    
+    const convertEvents = (serverEvents) => {
+        return serverEvents.map(event => ({
+        id: event._id,
+        title: event.title,
+        description: event.description,
+        start: new Date(event.start), 
+        end: new Date(event.start).setHours(new Date(event.start).getHours() + 1), 
+    }))};
+
+    const handleDateClick = async (info) => {
+        // info.dateStr will contain the date clicked in the format 'YYYY-MM-DD'
+        setSelectedDate(info.dateStr);
+        setSelectedEvent(null);
+        setIsEditing(false);
+        try {
+            const response = await fetch(`http://localhost:3001/events/date/${info.dateStr}`);
+            const events = await response.json();
+            const convertedEvents = convertEvents(events);
+            setDaysEvents(convertedEvents); // Set the events of the selected day
+            setModalVisible(true);
+        } catch (error) {
+            console.error("Error:", error);
+        }
+    };
+
+    const handleEventSubmit = (e) => {console.log('handleEventSubmit')
+        e.preventDefault();
+    
+        if (isEditing) {
+            const updatedEvent = {
+                title: selectedEvent.title,
+                start: selectedEvent.start,
+                description: selectedEvent.description,
+                starttime: selectedEvent.starttime,
+                ketatulos: selectedEvent.ketatulos,
+            };
+        
+            // Send a PUT request to the server
+            fetch(`http://localhost:3001/events/${selectedEvent.id}`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(updatedEvent),
+            })
+            .then(response => response.json())
+            .then(data => {
+                if (data.error) throw new Error(data.error);
+                console.log('Success:', data);
+                // Update the event in the events state
+                const updatedEvents = events.map(event => event.id === selectedEvent.id ? {...event, ...updatedEvent} : event);
+                setEvents(updatedEvents);
+                setModalVisible(false);
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+        } else {
+            const newEvent = {
+                title: selectedEvent.title,
+                start: selectedDate,
+                description: selectedEvent.description,
+                starttime: selectedEvent.starttime,
+                ketatulos: selectedEvent.ketatulos,
+                
+            };
+    
+            // Send a POST request to the server
+            fetch('http://localhost:3001/events', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(newEvent),
+            })
+            .then(response => response.json())
+            .then(data => {
+                if (data.error) throw new Error(data.error);
+                const createdEvent = data;
+                console.log('Success:', events);
+                const convertedEvents = convertEvents([createdEvent]);
+                setEvents([...events, ...convertedEvents]);
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+    
+            setModalVisible(false);
+        }
+    };
+
+    const fetchEvents = async () => {
+        fetch('http://localhost:3001/events')
+          .then(response => response.json())
+          .then(data => {
+            data.forEach(event => {
+                console.log(new Date(event.start))
+
+            })
+            const convertedEvents = convertEvents(data);
+            return setEvents(convertedEvents)})
+          .catch(error => console.error('Error:', error));
+    }
+
+    useEffect(() => {
+        if (!events) {
+            console.log('fetching events')
+            fetchEvents()
+        }
+        
+    }, [events]);
+
+    const changeInput = (key, value) => {
+        setSelectedEvent({ ...selectedEvent, [key]: value });
+    }
+
+    return (
+        <div>
+    <ReactModal
+    isOpen={modalVisible}
+    onRequestClose={() => setModalVisible(false)}
+    contentLabel="Event Details"
+    style={{
+        overlay: {
+            zIndex: 1000
+        }
+    }}
+>
+            
+                <h2>Event Details</h2>
+                <form onSubmit={handleEventSubmit}>
+                    <div>  
+                        <label>
+                            Title:
+                            <input type="text" value={(selectedEvent && selectedEvent.title) || ''} onChange={e => changeInput('title', e.target.value)} />
+                        </label>
+                    </div>
+                    <div>
+                        <label>
+                            Description:
+                            <input type="text" value={(selectedEvent && selectedEvent.description) || ''} onChange={e => changeInput('description', e.target.value)} />
+                        </label>
+                    </div>      
+                    <div>
+                        <label>
+                            Start time:
+                            <input type="text" value={(selectedEvent && selectedEvent.starttime) || ''} onChange={e => changeInput('starttime', e.target.value)} />
+                        </label>
+                    </div>      
+                    <div>
+                        <label>
+                            Ketä tulos:
+                            <input type="text" value={(selectedEvent && selectedEvent.ketatulos) || ''} onChange={e => changeInput('ketatulos', e.target.value)} />
+                        </label>
+                    </div>      
+                    <button type="submit" style={{backgroundColor: '#3a87ad', color: 'white'}}>
+                        {isEditing ? 'Save Changes' : 'Add Event'}
+                    </button>
+                </form>
+                <button onClick={() => setModalVisible(false)}>Close</button>
+                {isEditing &&<button onClick={handleDeleteEvent} style={{backgroundColor: '#f00', color: 'white'}}>Delete Event</button>}
+
+            </ReactModal>
+            <FullCalendar
+  ref={calendarRef}
+  plugins={[dayGridPlugin, interactionPlugin]}
+  displayEventTime={false}
+  initialView="dayGridMonth"
+  selectable={true}
+  events={events} // pass the events here
+  dateClick={handleDateClick}
+  eventClick={handleEventClick}
+  locale={fiLocale}
+/>
+        </div>
+    );
 }
 
-export default App;
+export default Calendar;
---
+++ src/serveri.js
@@ -0,0 +1,102 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose'); // Add this line
+const app = express();
+
+app.use(cors());
+
+// Declare the port variable once
+const port = process.env.PORT || 3001;
+
+app.use(express.json()); // for parsing application/json
+
+mongoose.connect('mongodb+srv://peolr:passu@cluster0.ne2nivb.mongodb.net/myDatabase', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB Atlas'))
+  .catch(err => console.error('Could not connect to MongoDB Atlas...', err));
+
+const eventSchema = new mongoose.Schema({
+  title: String,
+  start: Date,
+  description: String,
+  etatulos: String, // new property
+  starttime: String, // new property
+});
+
+const Event = mongoose.model('Event', eventSchema, 'calendarEvents');
+
+app.get('/events', async (req, res) => {
+  const events = await Event.find();
+  res.send(events);
+});
+app.get('/events/:id', async (req, res) => {
+if (!req.params.id || req.params.id === 'undefined') return res.status(400).json({ error: 'Missing id parameter' });
+  const id = req.params.id;
+  const event = await Event.findOne({ _id: id });
+  res.send(event); 
+});
+app.post('/events', (req, res) => {
+  const { title, start, description, ketatulos, starttime } = req.body;
+  const event = new Event({ title, start, description, ketatulos, starttime });
+
+  console.log(`Received new event: ${title}, ${start}, ${description}, ${ketatulos}, ${starttime}`);
+
+  event.save()
+    .then((newEvent) => {
+      console.log('Event saved to the database successfully: ', newEvent);
+      res.status(200).json(newEvent);
+    })
+    .catch(err => {
+      console.error('An error occurred while saving the event to the database:', err);
+      res.status(500).json({ error: 'An error occurred while creating the event' });
+    });
+});
+app.delete('/events/:id', async (req, res) => {
+  if (!req.params.id || req.params.id === 'undefined') return res.status(400).json({ error: 'Missing id parameter' });
+
+  const id = req.params.id;
+
+  try {
+    const event = await Event.findByIdAndDelete(id);
+
+    if (!event) return res.status(404).json({ error: 'Event not found' });
+
+    res.status(200).json({ message: 'Event deleted successfully' });
+  } catch (error) {
+    console.error('An error occurred while deleting the event:', error);
+    res.status(500).json({ error: 'An error occurred while deleting the event' });
+  }
+});
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
+});
+
+app.get('/events/date/:date', async (req, res) => {
+  const date = new Date(req.params.date);
+  const nextDate = new Date(date);
+  nextDate.setDate(date.getDate() + 1);
+
+  const events = await Event.find({
+      start: {
+          $gte: date,
+          $lt: nextDate
+      }
+  });
+
+  res.send(events);
+});
+
+app.put('/events/:id', async (req, res) => {
+  const updatedEvent = req.body;
+  try {
+      const event = await Event.findByIdAndUpdate(req.params.id, updatedEvent, { new: true });
+      res.json(event);
+  } catch (err) {
+      res.status(500).json({ error: err.message });
+  }
+
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
